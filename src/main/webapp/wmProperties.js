a62461a00417a4482c73c94eac3f774505a74a66
QmLbNkUbWQ <automate1@wavemaker.com>
2017-08-01 17:58:18 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_CmuOBRiuLW",
+  "homePage" : "Main",
+  "name" : "Auto_CmuOBRiuLW",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
